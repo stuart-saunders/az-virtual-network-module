45be2287ac36cd8b9b9d87ddbc4b4f5f4633f77b
Stuart Saunders <stuart-saunders@users.noreply.github.com>
2025-08-30 10:15:39 +0100

    Initial commit

---
+++ example/terraform.tf
@@ -0,0 +1,23 @@
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = ">=4.32.0"
+#     }
+
+#     azapi = {
+#       source  = "hashicorp/azapi"
+#       version = ">=2.5.0"
+#     }
+#   }
+# }
+
+# provider "azurerm" {
+#   # subscription_id = ""
+#   features {}
+# }
+
+# provider "azapi" {
+#   # subscription_id = ""
+#   use_msi         = false
+# }
